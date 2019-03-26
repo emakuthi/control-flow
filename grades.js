e451d74ab18ab50399b135121eee39f0f2aab613
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-03-26 14:14:17 +0300

    Programme that grades students

---
+++ grades.js
@@ -0,0 +1,13 @@
+//Programme that gives grades to the marks.
+var grade=prompt("what is your grade?");
+if(grade>=80){
+  alert("You are been graded as A student!");}
+  else if(grade>=60){
+    alert("You are been graded as B student!");}
+    else if(grade>=40){
+      alert("You are been graded as C student!");}
+      else if(grade>=20){
+        alert("You are been graded as D student!");}
+        else if(grade<20){
+          alert("You have been graded E");}
+    else{ alert("You have no grades")}
