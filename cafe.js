ff72a71f2d974bde4b234ab696539ef80fcf4267
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-03-26 13:44:45 +0300

    program which helps you choose btween coffee and Tea.!

---
+++ cafe.js
@@ -0,0 +1,7 @@
+//programme which prompts you to choose btw coffee and tea
+var favouriteBeaverage=prompt("Please enter coffee or tea :");
+if(favouriteBeaverage=="coffee"){
+  alert("your order for"+" "+favouriteBeaverage+" "+ "has been recieved!");}
+  else if(favouriteBeaverage=="tea"){
+    alert("your order for"+" "+favouriteBeaverage+" "+ "has been recieved!");}
+    else{ alert("please enter the coffee or tea!")}
