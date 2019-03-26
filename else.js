816b48c6aac31efa548b0466efa675e27fb96041
Elvis Nzioki Makuthi <EMakuthi@safaricom.co.ke>
2019-03-26 12:38:52 +0300

    the program check for even nummbers

---
+++ else.js
@@ -0,0 +1,7 @@
+
+//a programme which tells you the number you have entered is either even or not.
+
+var evenNumbers = prompt("Please enter a number: ");
+if (evenNumbers/2===0){
+alert(evenNumbers+"is an even number!");}
+else{alert(evenNumbers + " is not an evenNumber!")}
